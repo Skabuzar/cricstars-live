8c16bafd3cec2fa87421e13222b483cf4e378e11
Skabuzar <94217411+Skabuzar@users.noreply.github.com>
2026-02-07 21:04:14 +0530

    Update Firebase configuration API key

--- firebase.js
+++ firebase.js
@@ -3,14 +3,15 @@
 <script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
 
 <script>
-var firebaseConfig = {
-  apiKey: "AIzaSyBS9-1LxaOodnDKaxxvPur2Fo1FYfUP78",
-  authDomain: "cricstars-12.firebaseapp.com",
-  databaseURL: "https://cricstars-12-default-rtdb.firebaseio.com",
-  projectId: "cricstars-12",
-  storageBucket: "cricstars-12.appspot.com",
-  messagingSenderId: "451935240165",
-  appId: "1:451935240165:web:3edd4b421e6317b13b390c"
-};
-firebase.initializeApp(firebaseConfig);
+  var firebaseConfig = {
+    apiKey: "AIzaSyBS9-IlXao0dnDKaxxvPuFzO1YFyFUP78",
+    authDomain: "cricstars-12.firebaseapp.com",
+    databaseURL: "https://cricstars-12-default-rtdb.firebaseio.com",
+    projectId: "cricstars-12",
+    storageBucket: "cricstars-12.appspot.com",
+    messagingSenderId: "451935240165",
+    appId: "1:451935240165:web:3edd4b421e6317b13b390c"
+  };
+
+  firebase.initializeApp(firebaseConfig);
 </script>
