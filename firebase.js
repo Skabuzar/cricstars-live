a8b773aa0119cf9f1d7425ead3736df07b4589ab
Skabuzar <94217411+Skabuzar@users.noreply.github.com>
2026-02-07 13:45:05 +0530

    Add CricStars live scoring files

---
+++ firebase.js
@@ -0,0 +1,16 @@
+<!-- Firebase SDK -->
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
+
+<script>
+var firebaseConfig = {
+  apiKey: "AIzaSyBS9-IlXaoOdnDKaxxvpUr2Fo1YFyfUP78",
+  authDomain: "cricstars--12.firebaseapp.com",
+  databaseURL: "https://cricstars--12-default-rtdb.firebaseio.com",
+  projectId: "cricstars--12",
+  storageBucket: "cricstars--12.appspot.com",
+  messagingSenderId: "451935240165",
+  appId: "1:451935240165:web:3edd4b421e6317b13b390c"
+};
+firebase.initializeApp(firebaseConfig);
+</script>
