94a579cc1422480979181ab75fb48e0c0e2aaa69
Skabuzar <94217411+Skabuzar@users.noreply.github.com>
2026-02-07 20:24:54 +0530

    Refactor Firebase initialization with script tags
    Updated Firebase configuration to include script tags for SDKs and moved initialization code inside a script tag.

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
-// Firebase v8 config (simple & stable)
+<!-- Firebase SDKs -->
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
 
-var firebaseConfig = {
-  apiKey: "AIzaSyBS9-IlXaoOdnDKaxxvpUr2Fo1YFyfUP78",
-  authDomain: "cricstars--12.firebaseapp.com",
-  databaseURL: "https://cricstars--12-default-rtdb.firebaseio.com",
-  projectId: "cricstars--12",
-  storageBucket: "cricstars--12.appspot.com",
-  messagingSenderId: "451935240165",
-  appId: "1:451935240165:web:3edd4b421e6317b13b390c"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Database reference
-var db = firebase.database();
+<script>
+  var firebaseConfig = {
+    apiKey: "AIzaSyBS9-IlXaoOdnDKaxxvpUr2Fo1YFyfUP78",
+    authDomain: "cricstars--12.firebaseapp.com",
+    databaseURL: "https://cricstars--12-default-rtdb.firebaseio.com",
+    projectId: "cricstars--12",
+    storageBucket: "cricstars--12.appspot.com",
+    messagingSenderId: "451935240165",
+    appId: "1:451935240165:web:3edd4b421e6317b13b390c"
+  };
+  firebase.initializeApp(firebaseConfig);
+</script>
